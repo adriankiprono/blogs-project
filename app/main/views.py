1f13fb4dae50185347e8f5b73962e0750a948d2f
adriankiprono <tuimuradrian6@gmail.com>
2019-12-02 14:44:17 +0300

    add views and forms in main

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,3 @@
 from flask_login import login_required
+from flask import render_template,redirect,url_for,request,abort,flash
+from ..models import User,Post,Comment
--- config.py
+++ config.py
@@ -7,7 +7,9 @@ class Config:
     
     # QUOTE_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adriantuimur://qweasdzxc1234567890@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
     # UPLOADED_PHOTOS_DEST ='app/static/photos'
     
 class ProdConfig(Config):
