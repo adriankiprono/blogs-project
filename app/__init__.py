0fe1a9c649e49715db6f0619d626e7c8cad3e518
adriankiprono <tuimuradrian6@gmail.com>
2019-12-02 13:39:22 +0300

    add login

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-# from flask_login import LoginManager
+from flask_login import LoginManager
+
 # from flask_moment import  Moment
 # from flask_uploads import UploadSet,configure_uploads,IMAGES
 
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='mangoes'
+export MAIL_USERNAME='kappaahmed8@gmail.com'
+export MAIL_PASSWORD='KingKong9'
 
 python3.8 manage.py server
