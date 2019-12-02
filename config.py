b86295f8bcf8cb964bb4bf6f69002c3cd188e75c
adriankiprono <tuimuradrian6@gmail.com>
2019-12-02 12:31:16 +0300

    add cofiguration to config

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    # QUOTE_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    
+    Args:
+        The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+
+class TestConfig(Config):
+    pass
+
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:daniel@localhost/danteblog'
+    
+    DEBUG = True
+    
+    
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY='mangoes'
+
+python3.8 manage.py server
