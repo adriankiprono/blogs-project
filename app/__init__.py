03b32cd33ca3560d61c17674fe244e66afa64099
adriankiprono <tuimuradrian6@gmail.com>
2019-12-03 08:55:11 +0300

    add readme

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 # from flask_moment import  Moment
 # from flask_uploads import UploadSet,configure_uploads,IMAGES
@@ -11,7 +12,7 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 db =SQLAlchemy()
 bootstrap = Bootstrap()
 
-
+mail = Mail()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -37,7 +38,7 @@ def create_app(config_name):
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
-    # moment.init_app(app)
+    
     
     #setting config
     # from .requests import configure_request
